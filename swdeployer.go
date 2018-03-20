23f7d90bf32ae390fdb11fda86c2ee059d018572
Etienne Bruines <etienne.bruines@webcustoms.de>
2018-03-20 12:24:00 +0100

    Updated to 5.4.1

--- swdeployer.go
+++ swdeployer.go
@@ -175,6 +175,14 @@ const sw5 = "Shopware 5"
 var FalseVariable = false
 
 var compatibleSoftwareVersions = map[string]CompatibleSoftwareVersion{
+	"5.4.1": {
+		Checked:    &FalseVariable,
+		ID:         107,
+		Major:      sw5,
+		Name:       "5.4.1",
+		Parent:     105,
+		Selectable: true,
+	},
 	"5.4.0": {
 		Checked:    &FalseVariable,
 		ID:         106,
@@ -475,7 +483,7 @@ var compatibleSoftwareVersions = map[string]CompatibleSoftwareVersion{
 
 func compatibleVersions(from, to string) (versions []CompatibleSoftwareVersion) {
 	if to == "" {
-		to = "5.4.0"
+		to = "5.4.1"
 	}
 
 	if from == "" {
@@ -865,7 +873,7 @@ func main() {
 			Email: "etienne.bruines@webcustoms.de",
 		},
 	}
-	app.Version = "0.0.5.4.0"
+	app.Version = "0.0.5.4.1"
 	app.Action = func(c *cli.Context) error {
 		err := logic(c)
 		if err != nil {
